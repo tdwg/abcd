276b0b37bd3fa2af2c918ec3af30d31b0fc67d9e
Fabian Reimeier <f.reimeier@fu-berlin.de>
2019-02-27 15:17:20 +0100

    Support Internet Explorer

--- assets/custom/js/concepts.js
+++ assets/custom/js/concepts.js
@@ -1,132 +1,132 @@
 Vue.component("concept-table", {
   props: ["concept", "owltype"],
   template:
-    `
-    <table class="responsive-table concept-table">
-      <tbody>
-        <tr>
-          <td><strong>Term Name</strong></td>
-          <td>{{ concept['@rdf:about'] }} </td>
-        </tr>
-        <tr>
-          <td><strong>Label</strong></td>
-          <td>{{ concept['rdfs:label']['#text'] }} </td>
-        </tr>
-        <tr>
-          <td><strong>Controlled Value</strong></td>
-          <td>{{ concept['@rdf:about'] }}</td>
-        </tr>
-        <tr>
-          <td><strong>Defined By</strong></td>
-          <td>{{ concept['rdfs:isDefinedBy']['@rdf:resource'] }}</td>
-        </tr>
-        <tr>
-          <td><strong>Status</strong></td>
-          <td>{{ concept['dwcattributes:status'] }}</td>
-        </tr>
-        <tr>
-          <td><strong>Term IRI</strong></td>
-          <td><a v-bind:href="concept['rdfs:isDefinedBy']['@rdf:resource']+concept['@rdf:about']">{{ concept['rdfs:isDefinedBy']['@rdf:resource']+concept['@rdf:about'] }}</a></td>
-        </tr>
-        <tr>
-          <td><strong>Date Issued</strong></td>
-          <td>{{ concept['dcterms:issued'] }}</td>
-        </tr>
-        <tr>
-          <td><strong>Date Modified</strong></td>
-          <td>{{ concept['dcterms:modified'] }}</td>
-        </tr>
-        <tr>
-          <td><strong>Term Version IRI</strong></td>
-          <td><a v-bind:href="concept['rdfs:isDefinedBy']['@rdf:resource']+'history/'+concept['@rdf:about']+'_'+concept['dcterms:modified']">{{ concept['rdfs:isDefinedBy']['@rdf:resource']+'history/'+concept['@rdf:about']+'_'+concept['dcterms:modified'] }}</a></td>
-        </tr>
-        <tr>
-          <td><strong>Type</strong></td>
-          <!--<td>{{ owltype }}{{ concept.functional ? ', owl:FunctionalProperty' : '' }}</td>-->
-          <td>{{ owltype }}</td>
-        </tr>
-        <tr v-if="owltype=='owl:Class' && typeof concept['rdfs:subClassOf'] != 'undefined'">
-          <td><strong>Subclass Of</strong></td>
-          <td><a v-bind:href="concept['rdfs:isDefinedBy']['@rdf:resource']+concept['rdfs:subClassOf']['@rdf:resource']">{{ concept['rdfs:isDefinedBy']['@rdf:resource']+concept['rdfs:subClassOf']['@rdf:resource'] }}</a></td>
-        </tr>
-        <tr v-if="owltype=='owl:ObjectProperty' || owltype=='owl:DatatypeProperty'">
-          <td><strong>Has Domain</strong></td>
-          <td>{{ concept['rdfs:domain']['@rdf:resource'] }}</td>
-        </tr>
-        <tr v-if="owltype=='owl:ObjectProperty' || owltype=='owl:DatatypeProperty'">
-          <td><strong>Has Range</strong></td>
-          <td>{{ concept['rdfs:range']['@rdf:resource'] }}</td>
-        </tr>
-        <tr v-if="owltype=='owl:NamedIndividual'">
-          <td><strong>Instance Of</strong></td>
-          <td>{{ concept.inVocabulary }}</td>
-        </tr>
-        <tr>
-          <td><strong>Comment</strong></td>
-          <td>{{ concept['skos:definition'] }}</td>
-        </tr>
-      </tbody>
-    </table>
-    `
+    "\
+    <table class=\"responsive-table concept-table\">\
+      <tbody>\
+        <tr>\
+          <td><strong>Term Name</strong></td>\
+          <td>{{ concept['@rdf:about'] }} </td>\
+        </tr>\
+        <tr>\
+          <td><strong>Label</strong></td>\
+          <td>{{ concept['rdfs:label']['#text'] }} </td>\
+        </tr>\
+        <tr>\
+          <td><strong>Controlled Value</strong></td>\
+          <td>{{ concept['@rdf:about'] }}</td>\
+        </tr>\
+        <tr>\
+          <td><strong>Defined By</strong></td>\
+          <td>{{ concept['rdfs:isDefinedBy']['@rdf:resource'] }}</td>\
+        </tr>\
+        <tr>\
+          <td><strong>Status</strong></td>\
+          <td>{{ concept['dwcattributes:status'] }}</td>\
+        </tr>\
+        <tr>\
+          <td><strong>Term IRI</strong></td>\
+          <td><a v-bind:href=\"concept['rdfs:isDefinedBy']['@rdf:resource']+concept['@rdf:about']\">{{ concept['rdfs:isDefinedBy']['@rdf:resource']+concept['@rdf:about'] }}</a></td>\
+        </tr>\
+        <tr>\
+          <td><strong>Date Issued</strong></td>\
+          <td>{{ concept['dcterms:issued'] }}</td>\
+        </tr>\
+        <tr>\
+          <td><strong>Date Modified</strong></td>\
+          <td>{{ concept['dcterms:modified'] }}</td>\
+        </tr>\
+        <tr>\
+          <td><strong>Term Version IRI</strong></td>\
+          <td><a v-bind:href=\"concept['rdfs:isDefinedBy']['@rdf:resource']+'history/'+concept['@rdf:about']+'_'+concept['dcterms:modified']\">{{ concept['rdfs:isDefinedBy']['@rdf:resource']+'history/'+concept['@rdf:about']+'_'+concept['dcterms:modified'] }}</a></td>\
+        </tr>\
+        <tr>\
+          <td><strong>Type</strong></td>\
+          <!--<td>{{ owltype }}{{ concept.functional ? ', owl:FunctionalProperty' : '' }}</td>-->\
+          <td>{{ owltype }}</td>\
+        </tr>\
+        <tr v-if=\"owltype=='owl:Class' && typeof concept['rdfs:subClassOf'] != 'undefined'\">\
+          <td><strong>Subclass Of</strong></td>\
+          <td><a v-bind:href=\"concept['rdfs:isDefinedBy']['@rdf:resource']+concept['rdfs:subClassOf']['@rdf:resource']\">{{ concept['rdfs:isDefinedBy']['@rdf:resource']+concept['rdfs:subClassOf']['@rdf:resource'] }}</a></td>\
+        </tr>\
+        <tr v-if=\"owltype=='owl:ObjectProperty' || owltype=='owl:DatatypeProperty'\">\
+          <td><strong>Has Domain</strong></td>\
+          <td>{{ concept['rdfs:domain']['@rdf:resource'] }}</td>\
+        </tr>\
+        <tr v-if=\"owltype=='owl:ObjectProperty' || owltype=='owl:DatatypeProperty'\">\
+          <td><strong>Has Range</strong></td>\
+          <td>{{ concept['rdfs:range']['@rdf:resource'] }}</td>\
+        </tr>\
+        <tr v-if=\"owltype=='owl:NamedIndividual'\">\
+          <td><strong>Instance Of</strong></td>\
+          <td>{{ concept.inVocabulary }}</td>\
+        </tr>\
+        <tr>\
+          <td><strong>Comment</strong></td>\
+          <td>{{ concept['skos:definition'] }}</td>\
+        </tr>\
+      </tbody>\
+    </table>\
+    "
 });
 
 
 Vue.component("associated-properties", {
   props: ["concept"],
   template: 
-    `
-    <div class="associated-properties">
-      <template v-if="Array.isArray(concept['ns0:associatedProperty'])">
-        <a v-for="prop in concept['ns0:associatedProperty']" class="associated-property waves-effect waves-light btn btn-flat" v-bind:href="'#'+prop['@rdf:resource']">{{ prop['@rdf:resource'] }}</a>
-      </template>
-      <template v-if="concept['ns0:associatedProperty'] && !Array.isArray(concept['ns0:associatedProperty'])">
-        <a class="associated-property waves-effect waves-light btn btn-flat" v-bind:href="'#'+concept['ns0:associatedProperty']['@rdf:resource']">{{ concept['ns0:associatedProperty']['@rdf:resource'] }}</a>
-      </template>
-    </div>
-    `
+    "\
+    <div class=\"associated-properties\">\
+      <template v-if=\"Array.isArray(concept['ns0:associatedProperty'])\">\
+        <a v-for=\"prop in concept['ns0:associatedProperty']\" class=\"associated-property waves-effect waves-light btn btn-flat\" v-bind:href=\"'#'+prop['@rdf:resource']\">{{ prop['@rdf:resource'] }}</a>\
+      </template>\
+      <template v-if=\"concept['ns0:associatedProperty'] && !Array.isArray(concept['ns0:associatedProperty'])\">\
+        <a class=\"associated-property waves-effect waves-light btn btn-flat\" v-bind:href=\"'#'+concept['ns0:associatedProperty']['@rdf:resource']\">{{ concept['ns0:associatedProperty']['@rdf:resource'] }}</a>\
+      </template>\
+    </div>\
+    "
 });
 
 Vue.component("concept-card", {
   props: ["concept", "owltype"],
   template: 
-  	`
-  	<div class="card concept-card" v-bind:id="concept['@rdf:about']">
-    	<div class="card-content">
-    		<div class="card-title">
-          <div class="concept-label">{{ concept['rdfs:label']['#text'] }}</div>
-          <div class="concept-type">
-    		    <div v-if="owltype=='owl:Class'"><span class="new badge red" data-badge-caption="Class"></span></div>
-            <div v-if="owltype=='owl:ObjectProperty'"><span class="new badge blue" data-badge-caption="Object Property"></span></div>
-            <div v-if="owltype=='owl:DatatypeProperty'"><span class="new badge orange" data-badge-caption="Datatype Property"></span></div>
-            <!--<div v-if="owltype!='owl:Class' && concept['functional']"><span class="new badge grey" data-badge-caption="Functional Property"></span></div>-->
-    		    <div v-if="owltype=='owl:NamedIndividual'"><span class="new badge teal" data-badge-caption="Controlled Term"></span></div>
-          </div>
-        </div>
-        <p class="concept-definition">{{ concept['rdfs:comment'] }}</p>
-        <div class="concept-info">
-          <ul class="collapsible">
-            <li>
-              <div class="collapsible-header"><i class="material-icons">info</i>More information</div>
-              <div class="collapsible-body">
-                <keep-alive>
-                  <concept-table v-bind:concept="concept" v-bind:owltype="owltype"></concept-table>
-                </keep-alive>
-              </div>
-            </li>
-          </ul>
-  	    </div>
-        <template v-if="owltype=='owl:Class'">
-          <p>Associated Properties:</p>
-          <keep-alive>
-            <associated-properties v-bind:concept="concept"></associated-properties>
-          </keep-alive>
-        </template>
-  	  </div>
-    	<div class="card-action">
-  	  	<a class="waves-effect waves-light btn green" v-bind:href="concept['rdfs:isDefinedBy']['@rdf:resource']+concept['@rdf:about']">2019-01-31 (Current)</a>
-    	</div>
-    </div>
-  	`
+    "\
+    <div class=\"card concept-card\" v-bind:id=\"concept['@rdf:about']\">\
+      <div class=\"card-content\">\
+        <div class=\"card-title\">\
+          <div class=\"concept-label\">{{ concept['rdfs:label']['#text'] }}</div>\
+          <div class=\"concept-type\">\
+            <div v-if=\"owltype=='owl:Class'\"><span class=\"new badge red\" data-badge-caption=\"Class\"></span></div>\
+            <div v-if=\"owltype=='owl:ObjectProperty'\"><span class=\"new badge blue\" data-badge-caption=\"Object Property\"></span></div>\
+            <div v-if=\"owltype=='owl:DatatypeProperty'\"><span class=\"new badge orange\" data-badge-caption=\"Datatype Property\"></span></div>\
+            <!--<div v-if=\"owltype!='owl:Class' && concept['functional']\"><span class=\"new badge grey\" data-badge-caption=\"Functional Property\"></span></div>-->\
+            <div v-if=\"owltype=='owl:NamedIndividual'\"><span class=\"new badge teal\" data-badge-caption=\"Controlled Term\"></span></div>\
+          </div>\
+        </div>\
+        <p class=\"concept-definition\">{{ concept['rdfs:comment'] }}</p>\
+        <div class=\"concept-info\">\
+          <ul class=\"collapsible\">\
+            <li>\
+              <div class=\"collapsible-header\"><i class=\"material-icons\">info</i>More information</div>\
+              <div class=\"collapsible-body\">\
+                <keep-alive>\
+                  <concept-table v-bind:concept=\"concept\" v-bind:owltype=\"owltype\"></concept-table>\
+                </keep-alive>\
+              </div>\
+            </li>\
+          </ul>\
+        </div>\
+        <template v-if=\"owltype=='owl:Class'\">\
+          <p>Associated Properties:</p>\
+          <keep-alive>\
+            <associated-properties v-bind:concept=\"concept\"></associated-properties>\
+          </keep-alive>\
+        </template>\
+      </div>\
+      <div class=\"card-action\">\
+        <a class=\"waves-effect waves-light btn green\" v-bind:href=\"concept['rdfs:isDefinedBy']['@rdf:resource']+concept['@rdf:about']\">2019-01-31 (Current)</a>\
+      </div>\
+    </div>\
+    "
 });
 
 $app = new Vue({
@@ -174,8 +174,8 @@ $app = new Vue({
 	methods: {
     getConcepts() {
       var vinstance = this;
-      /*$.getJSON('/terms/abcdTerms.json', (json) => {
-        json.forEach((concept) => vinstance.concepts.push(concept));
+      /*$.getJSON('/terms/abcdTerms.json', function(json){
+        json.forEach(function(concept){ vinstance.concepts.push(concept) });
         vinstance.handleAnchor=true;
       });*/
       $.ajax({
@@ -184,32 +184,32 @@ $app = new Vue({
         async: false,
         success: function (json) {
           
-          json["rdf:RDF"]["owl:Class"].map( concept => {
+          json["rdf:RDF"]["owl:Class"].map(function(concept){
             concept.owltype="owl:Class";
             vinstance.concepts.push(concept);
           });
 
-          var functionalProps = json["rdf:RDF"]["owl:FunctionalProperty"].map( concept => concept["@rdf:about"] );
+          var functionalProps = json["rdf:RDF"]["owl:FunctionalProperty"].map(function(concept){ return concept["@rdf:about"]; });
 
-          json["rdf:RDF"]["owl:DatatypeProperty"].map( concept => {
+          json["rdf:RDF"]["owl:DatatypeProperty"].map(function(concept){
             concept.owltype = "owl:DatatypeProperty";
-            concept.functional = functionalProps.includes(concept["@rdf:about"]); 
+            concept.functional = _.includes(functionalProps, concept["@rdf:about"]); 
             concept["vann:termGroup"] = json["rdf:RDF"]["propertyConceptGroups"][concept["@rdf:about"]];
             vinstance.concepts.push(concept);
           });
-          json["rdf:RDF"]["owl:ObjectProperty"].map( concept => {
+          json["rdf:RDF"]["owl:ObjectProperty"].map(function(concept){
             concept.owltype = "owl:ObjectProperty";
-            concept.functional = functionalProps.includes(concept["@rdf:about"]); 
+            concept.functional = _.includes(functionalProps, concept["@rdf:about"]);  
             concept["vann:termGroup"] = json["rdf:RDF"]["propertyConceptGroups"][concept["@rdf:about"]];
             vinstance.concepts.push(concept);
           });
-          _.keys(json["rdf:RDF"]).map( key => {
+          _.keys(json["rdf:RDF"]).map(function(key){
             if(key.startsWith("abcd:")){
-              json["rdf:RDF"][key].map( concept => {
+              json["rdf:RDF"][key].map(function(concept){
                 concept.owltype = "owl:NamedIndividual";
                 concept.inVocabulary = key;
                 concept['rdfs:label'] = {"@xml:lang": "en", "#text": concept["@rdf:about"]};
-                concept.functional = functionalProps.includes(concept["@rdf:about"]); 
+                concept.functional = _.includes(functionalProps, concept["@rdf:about"]); 
                 concept["vann:termGroup"] = json["rdf:RDF"]["propertyConceptGroups"][concept["@rdf:about"]];
                 vinstance.concepts.push(concept);
               });
@@ -222,9 +222,9 @@ $app = new Vue({
     },
     getConceptGroup(conceptGroup){
       groupConcepts = {};
-      var filter = (concept) => {
+      var filter = function(concept){
         if(Array.isArray(concept["vann:termGroup"])) 
-          return concept["vann:termGroup"].reduce((acc, group) => acc || group["@rdf:resource"] == conceptGroup, false);
+          return concept["vann:termGroup"].reduce(function(acc, group){ return acc || group["@rdf:resource"] == conceptGroup; }, false);
         return concept["vann:termGroup"]["@rdf:resource"] == conceptGroup;
       };
       groupConcepts["owl:Class"] = _.sortBy(this.classes.filter(filter), '@rdf:about');
@@ -253,19 +253,19 @@ $app = new Vue({
 	},
 	computed: {
     classes: function() {
-      return this.filteredConcepts.filter( concept => concept.owltype == "owl:Class");
+      return this.filteredConcepts.filter(function(concept){ return concept.owltype == "owl:Class"; });
     },
     objectprops: function() {
-      return this.filteredConcepts.filter( concept => concept.owltype == "owl:ObjectProperty");
+      return this.filteredConcepts.filter(function(concept){ return concept.owltype == "owl:ObjectProperty" });
     },
     datatypeprops: function() {
-      return this.filteredConcepts.filter( concept => concept.owltype == "owl:DatatypeProperty");
+      return this.filteredConcepts.filter(function(concept){ return concept.owltype == "owl:DatatypeProperty" });
     },
     controlledTerms: function() {
-      return this.filteredConcepts.filter( concept => concept.owltype == "owl:NamedIndividual");
+      return this.filteredConcepts.filter(function(concept){ return concept.owltype == "owl:NamedIndividual" });
     },
     conceptGroups: function () {
-      return _.uniq(this.filteredConcepts.map(concept =>Array.isArray(concept["vann:termGroup"]) ? concept["vann:termGroup"].map(elem => elem["@rdf:resource"]) : concept["vann:termGroup"]["@rdf:resource"]).flat()).sort();
+      return _.uniq(_.flatten(this.filteredConcepts.map(function(concept){ return Array.isArray(concept["vann:termGroup"]) ? concept["vann:termGroup"].map(function(elem){ return elem["@rdf:resource"]; }) : concept["vann:termGroup"]["@rdf:resource"] }))).sort();
     }
 	}
 });
--- assets/custom/js/navigation.js
+++ assets/custom/js/navigation.js
@@ -1,41 +1,41 @@
 Vue.component("navigation", {
   props:["title"],
   template: 
-    `
-    <div id="navigation">
-      <nav class="top-nav">
-        <div class="container">
-          <div class="nav-wrapper">
-            <div class="row">
-              <div class="col s12 m10 offset-m1">
-                <h1 class="header">{{ title }}</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-      <div class="container"><a href="#" data-target="menu" class="top-nav sidenav-trigger full hide-on-large-only"><i class="material-icons">menu</i></a></div>
-      <ul id="menu" class="sidenav sidenav-fixed show-on-large">
-        <li class="title"><h3 class="center-align">ABCD</h3></li>
-        <li class="title"><h5 class="center-align">Access to Biological Collections Data</h5></li>
-        <li class="divider"></li>
-        <li class="bold menu-item sidenav-close" v-bind:class="{ active: currentUrl=='/' }"><a href="/" class="waves-effect waves-green">Home</a></li>
-        <li class="bold menu-item sidenav-close" v-bind:class="{ active: currentUrl=='/3.0/' }"><a href="/3.0" class="waves-effect waves-green">ABCD 3.0</a></li>
-        <li class="bold menu-item sidenav-close" v-bind:class="{ active: currentUrl=='/xml/' }"><a href="/xml" class="waves-effect waves-green">XML</a>
-          <ul class="submenu">
-            <li class="bold menu-item sidenav-close" v-bind:class="{ active: currentUrl=='/xml/documentation/primer/' }"><a href="/xml/documentation/primer" class="waves-effect waves-green">Primer</a></li>
-            <li class="bold menu-item sidenav-close" v-bind:class="{ active: currentUrl=='/xml/documentation/changes/' }"><a href="/xml/documentation/changes" class="waves-effect waves-green">ABCD 3.0 Changes</a></li>
-          </ul>
-        </li>
-        <li class="bold menu-item sidenav-close" v-bind:class="{ active: currentUrl=='/ontology/' }"><a href="/ontology" class="waves-effect waves-green">Ontology</a>
-          <ul class="submenu">
-            <li class="bold menu-item sidenav-close" v-bind:class="{ active: currentUrl=='/ontology/documentation/primer/' }"><a href="/ontology/documentation/primer" class="waves-effect waves-green">Primer</a></li>
-            <li class="bold menu-item sidenav-close" v-bind:class="{ active: currentUrl=='/terms/'}"><a href="/terms" class="waves-effect waves-green">Terms</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-    `
+    "\
+    <div id=\"navigation\">\
+      <nav class=\"top-nav\">\
+        <div class=\"container\">\
+          <div class=\"nav-wrapper\">\
+            <div class=\"row\">\
+              <div class=\"col s12 m10 offset-m1\">\
+                <h1 class=\"header\">{{ title }}</h1>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </nav>\
+      <div class=\"container\"><a href=\"#\" data-target=\"menu\" class=\"top-nav sidenav-trigger full hide-on-large-only\"><i class=\"material-icons\">menu</i></a></div>\
+      <ul id=\"menu\" class=\"sidenav sidenav-fixed show-on-large\">\
+        <li class=\"title\"><h3 class=\"center-align\">ABCD</h3></li>\
+        <li class=\"title\"><h5 class=\"center-align\">Access to Biological Collections Data</h5></li>\
+        <li class=\"divider\"></li>\
+        <li class=\"bold menu-item sidenav-close\" v-bind:class=\"{ active: currentUrl=='/' }\"><a href=\"/\" class=\"waves-effect waves-green\">Home</a></li>\
+        <li class=\"bold menu-item sidenav-close\" v-bind:class=\"{ active: currentUrl=='/3.0/' }\"><a href=\"/3.0\" class=\"waves-effect waves-green\">ABCD 3.0</a></li>\
+        <li class=\"bold menu-item sidenav-close\" v-bind:class=\"{ active: currentUrl=='/xml/' }\"><a href=\"/xml\" class=\"waves-effect waves-green\">XML</a>\
+          <ul class=\"submenu\">\
+            <li class=\"bold menu-item sidenav-close\" v-bind:class=\"{ active: currentUrl=='/xml/documentation/primer/' }\"><a href=\"/xml/documentation/primer\" class=\"waves-effect waves-green\">Primer</a></li>\
+            <li class=\"bold menu-item sidenav-close\" v-bind:class=\"{ active: currentUrl=='/xml/documentation/changes/' }\"><a href=\"/xml/documentation/changes\" class=\"waves-effect waves-green\">ABCD 3.0 Changes</a></li>\
+          </ul>\
+        </li>\
+        <li class=\"bold menu-item sidenav-close\" v-bind:class=\"{ active: currentUrl=='/ontology/' }\"><a href=\"/ontology\" class=\"waves-effect waves-green\">Ontology</a>\
+          <ul class=\"submenu\">\
+            <li class=\"bold menu-item sidenav-close\" v-bind:class=\"{ active: currentUrl=='/ontology/documentation/primer/' }\"><a href=\"/ontology/documentation/primer\" class=\"waves-effect waves-green\">Primer</a></li>\
+            <li class=\"bold menu-item sidenav-close\" v-bind:class=\"{ active: currentUrl=='/terms/'}\"><a href=\"/terms\" class=\"waves-effect waves-green\">Terms</a></li>\
+          </ul>\
+        </li>\
+      </ul>\
+    </div>\
+    "
 });
 
 new Vue({
